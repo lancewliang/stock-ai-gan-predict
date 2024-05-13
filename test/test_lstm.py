21d2bd8b6e580aa406ff3f687273e20ed52d81cf
lancewliang <78599194@qq.com>
2024-05-13 18:44:50 +0800

    1

--- test/test_lstm.py
+++ test/test_lstm.py
@@ -1,24 +1,63 @@
+
 import torch  
 import torch.nn as nn  
-from torch.utils.data import Dataset, DataLoader  
-import numpy as np  
 import torch.optim as optim  
+from sklearn.metrics import accuracy_score  
+import matplotlib.pyplot as plt  
 import numpy as np  
 import pandas as pd
+from torch.utils.data import Dataset, DataLoader  
 
-
-# 假设你已经有了一个股票数据集的numpy数组，每个元素都是一个一维向量（如股票价格的历史）  
-# 这里我们模拟一些数据  
-np.random.seed(0)  
-n_samples = 1000  # 样本数量  
-seq_length = 22   # 每个样本的序列长度（例如，10天的股票价格）  
-n_features = 1    # 特征数量（例如，只有收盘价）  
-n_classes = 6     # 类别数量（例如，上涨/下跌）  
-# 创建一个DataLoader  
-batch_size = 32  
-num_epochs=1000
-# 指定使用的设备（GPU或CPU）  
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  
+
+
+
+# 定义超参数  
+input_size = 19  # 因子X的特征数量  
+hidden_size = 64  # LSTM隐藏层大小  
+num_layers = 1  # LSTM层数  
+num_classes=6 #涨跌
+
+# output_size_reg = 1  # 标签Y的回归特征数量 
+num_epochs = 2000  # 训练轮数  
+batch_size = 64  # 批处理大小  
+learning_rate = 0.001  # 学习率  
+  
+# 生成训练和测试数据  
+# num_samples = 2  # 生成1000个样本  
+sequence_length = 3  # 每个样本有10个时间步长  
+
+# LSTM模型定义  
+class LSTMClassifier(nn.Module):  
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):  
+        super(LSTMClassifier, self).__init__()  
+        self.hidden_size = hidden_size  
+        self.num_layers = num_layers  
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)  
+        self.fc = nn.Linear(hidden_size, num_classes) 
+  
+    def forward(self, x):  
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)  
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)  
+        out, _ = self.lstm(x, (h0, c0))  
+        out = out[:, -1, :]
+        
+        class_output = self.fc(out)
+        return class_output 
+    
+    
+ 
+# 初始化模型、损失函数和优化器  
+model = LSTMClassifier(input_size, hidden_size, num_layers, output_size_class)  
+ 
+criterion = nn.CrossEntropyLoss()  # 二损失函数，用于多标签分类  
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)  
+  
+# 将模型和数据移动到GPU（如果有的话），否则使用CPU  
+
+model = model.to(device)  
+
+
 
 root = "/home/lanceliang/cdpwork/ai/ai-stock/stockai/"
 def z_score_scaler(data):  
@@ -44,11 +83,7 @@ def transfer2dTo3dWithSequenceLen(sequence_length,df):
         current_slice = df.iloc[start_idx:end_idx]  
          
         # 将数据转换为NumPy数组  
-        numpy_array = current_slice[['收盘']].astype('float32').values  
-        
-        
-        
-        
+        numpy_array = current_slice[['收盘']].astype('float32').values   
         # 如果你需要，可以在这里对数据进行进一步处理（例如，标准化、归一化等）  
         
         # 将NumPy数组添加到列表中  
@@ -88,9 +123,11 @@ def load_traning_data(sequence_length):
     
     return X.to(device) , Y, X_test.to(device) , Y_test
 
-  
+
+
+
 # 将numpy数组转换为PyTorch张量  
-X_train, y_train,X_test, y_test = load_traning_data(seq_length)
+X_train, y_train,X_test, y_test = load_traning_data(sequence_length)
 
 # 定义一个简单的Dataset类  
 class StockDataset(Dataset):  
@@ -114,100 +151,7 @@ test_dataset = StockDataset(X_test, y_test)
 test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)  
 
 
-  
 
-# 自定义自注意力层  
-
-class SelfAttention(nn.Module):  
-
-    def __init__(self, embed_size, attention_dim):  
-        self.embed_size=embed_size
-        self.embedding_dim=embed_size
-        self.attention_dim=attention_dim
-        super(SelfAttention, self).__init__()  
-        self.query = nn.Linear(embed_size, embed_size // 8)  
-        self.key = nn.Linear(embed_size, embed_size // 8)  
-        self.value = nn.Linear(embed_size, embed_size)  
-        self.gamma = nn.Parameter(torch.zeros(1))  
-    def forward(self, x):  
-        batch_size, seq_len, embed_size = x.size()  
-  
-        # 计算query, key, value  
-        query = self.query(x).view(batch_size, seq_len, -1)  
-        key = self.key(x).view(batch_size, seq_len, -1)  
-        value = self.value(x).view(batch_size, seq_len, -1)  
-  
-        # 计算attention分数  
-        attention_scores = torch.matmul(query, key.transpose(-2, -1))  
-        attention_scores = attention_scores / torch.sqrt(torch.tensor(embed_size, dtype=torch.float32))  
-  
-        # 应用softmax得到attention权重  
-        attention_weights = nn.functional.softmax(attention_scores, dim=-1)  
-  
-        # 应用attention权重到value  
-        context = torch.matmul(attention_weights, value)  
-  
-        # 对序列所有位置的上下文表示取平均，或可以选择其他聚合方法  
-        context = context.mean(dim=1)  
-  
-        return context  
-
-
-class StockCNN(nn.Module):  
-    def __init__(self, input_size, num_classes):  
-        super(StockCNN, self).__init__()  
-        self.cnn = nn.Sequential(
-            nn.Conv1d(input_size, 64, 1, 1, 1),  # [64, 128, 128]
-            # nn.BatchNorm1d(32),
-            nn.ReLU(),
-            nn.MaxPool1d(2, 2, 0),      # [64, 64, 64]
-
-            nn.Conv1d(64, 128, 1, 1, 1), # [128, 64, 64]
-            # nn.BatchNorm1d(64),
-            nn.ReLU(),
-            nn.MaxPool1d(2, 2, 0),      # [128, 32, 32]
-
-            nn.Conv1d(128, 256, 1, 1, 1), # [256, 32, 32]
-            # nn.BatchNorm1d(128),
-            nn.ReLU(),
-            nn.MaxPool1d(2, 2, 0),      # [256, 16, 16]
-            
-        )
-        self.attention = SelfAttention(256,256)  # 假设SelfAttention是一个自定义层
-        self.fc = nn.Sequential(
-            nn.Linear(256, 512),
-            nn.ReLU(),
-            nn.Linear(512, 128),
-            nn.ReLU(),
-            nn.Linear(128, num_classes)
-        )
-
-    def forward(self, x):
-        # print(x.size())
-        out = self.cnn(x)
-        # print (out.size()) 
-        # 展平卷积后的特征图，准备输入到自注意力层  
-        out = out.transpose(1, 2)  
-        # print (out.size()) 
-        out = out.contiguous().view(out.size(0), -1, out.size(2))  
-        # print (out.size()) 
-        out = self.attention(out)  
-        #print (out.size())
-        # out = out.view(out.size()[0], -1)
-        # print (out.size())
-        return self.fc(out)
-   
-   
-   
-    
-# 实例化模型  
-
-model = StockCNN(seq_length, n_classes)  
-
-# 定义损失函数和优化器  
-criterion = nn.CrossEntropyLoss()  
-optimizer = optim.Adam(model.parameters(), lr=0.001)  
-  
 # 训练模型  
 def train(model, device, train_loader, optimizer, criterion, num_epochs=25):  
     model.train()  
